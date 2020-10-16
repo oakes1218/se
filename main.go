a5003a5aac38dfd24a38ac4afd3f6dd4a1335e08
EddieLe <eddie_lee@mail.chungyo.net>
2020-10-16 11:48:24 +0800

    init

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const tgtoken = "1394548836:AAGkTGCSVu7wD4Cgc6AQtseVn38r-Iv5cU4"
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(tgtoken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		log.Println("command:", update.Message.Command())
+
+		switch update.Message.Command() {
+		case "eddie":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+			fmt.Println("command:", update.Message.Command())
+			bot.Send(msg)
+		default:
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "error cmd")
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
+		}
+	}
+}
